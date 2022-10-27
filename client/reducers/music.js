7c3825b628c6f8175bff89cfb5a83e3d349cbd1d
Eamonn O'Sullivan <eamonnsosullivan@gmail.com>
2022-10-28 09:06:57 +1300

    fix reducers

--- client/reducers/music.js
+++ client/reducers/music.js
@@ -1,9 +1,4 @@
-import {
-  FETCH_MUSIC_SUCCESS,
-  ADD_ALBUM,
-  UPDATE_ALBUM,
-  DEL_ALBUM,
-} from '../actions/music'
+import { FETCH_MUSIC_SUCCESS } from '../actions/music'
 
 const initialState = []
 
@@ -11,14 +6,6 @@ const reducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_MUSIC_SUCCESS:
       return action.payload
-    case ADD_ALBUM:
-      return [...state, action.payload]
-    case UPDATE_ALBUM:
-      return [...state].map((item) =>
-        item.id == action.payload.id ? (item = action.payload) : item
-      )
-    case DEL_ALBUM:
-      return [...state].filter((item) => item.id !== action.payload)
     default:
       return state
   }
